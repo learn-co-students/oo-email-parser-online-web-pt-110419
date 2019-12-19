8291022bc1d86b9501b91537e9c6614e90bea609
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-19 17:07:59 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,7 +1,7 @@
 class email_addresses
   attr_accessor :email_addresses
   email_addresses = "john@doe.com", "person@somwhere.org"
-  parser = EmaiAddressParser.new(email_addresses)
+  parser = EmailAddressParser.new(email_addresses)
   
   parser.parse
   
